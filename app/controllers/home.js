2da62b434eb91fbe79940ed639be3d1477d2d0d4
Edgar Allan Glez <edgarallan182@gmail.com>
2016-09-19 16:56:15 -0600

    the init configuration for nodeJS/express/handlebars/postgresql

---
+++ app/controllers/home.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+  router = express.Router(),
+  db = require('../models');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+  db.Article.findAll().then(function (articles) {
+    debugger
+    res.render('index', {
+      title: 'Generator-Express MVC',
+      articles: articles[0].title
+    });
+  });
+});
+
+router.get('/users/:userId', function (req, res, next) {
+  db.Article.findAll().then(function (articles) {
+    res.render('index', {
+      title: 'Edgar Website ' + req.params.userId,
+      articles: articles[0].url
+    });
+    res.json({ user: 'EdgarAllanGlez' });
+  });
+});
---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'foxagram'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgresql://postgres:doprocks@45.55.7.118:5432/foxagramdb'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'foxagram'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/foxagram-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'foxagram'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/foxagram-production'
+  }
+};
+
+module.exports = config[env];
