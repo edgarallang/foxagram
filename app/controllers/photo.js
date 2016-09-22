b4d3acd1366a28a907e618e1bc19f898eedcb3ef
jequintero <eduardo.quintero52@gmail.com>
2016-09-22 09:53:48 -0600

    Photo upload

--- app/controllers/photo.js
+++ app/controllers/photo.js
@@ -1,6 +1,18 @@
 var express = require('express'),
   router = express.Router(),
-  db = require('../models');
+  db = require('../models'),
+  multer = require('multer'),
+  storage = multer.diskStorage({
+	  destination: function (req, file, callback) {
+	    callback(null, './uploads');
+	  },
+	  filename: function (req, file, callback) {
+	    callback(null, file.fieldname + '-' + Date.now());
+	  }
+	}),
+  upload = multer({ storage : storage, dest: 'photos/'}).single('userPhoto');
+
+
 
 module.exports = function (app) {
   app.use('/photo', router);
@@ -17,15 +29,15 @@ router.get('/by/id/:photoId', function(req, res){
 		}
 	});
 });
-
-router.get('/upload', function(req, res){
-	db.Photo.findOne({
-		where: { id: req.params.photoId }
-	}).then(function (photo){
-		if(!photo){
-			res.json({message: 'photo doesnt exist' })
-		}else{
-			res.json({message: photo})
+router.get('/upload', function (req, res, next) {
+	upload(req,res,function(err) {
+        if(err) {
+            return res.json("Error uploading photo.");
+        }else{
+			res.json({message:  'Photo uploaded' });
 		}
-	});
+    });
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
 });
+
